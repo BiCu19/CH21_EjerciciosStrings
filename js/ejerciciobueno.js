066d7f13bfdf69f45c37fd45f08931fe8e483273
bianca <cunaarely@gmail.com>
2022-12-12 13:51:38 -0600

    Se soluciono el primer ejercicio con String

---
+++ js/ejerciciobueno.js
@@ -0,0 +1,23 @@
+let nombre = "Bianca Arely Perez Cuna";
+nombre = nombre.toUpperCase();
+let letra ="";
+let contFinal=0;
+
+for (let index = 0; index < nombre.length; index++) {
+    console.log (nombre.charAt(index));
+    let contTemp=1;
+    for (let cont = index+ 1; cont < nombre.length; cont++) {
+        if(nombre.charAt(index)==nombre.charAt(cont))
+           contTemp++
+        
+        
+    } // for cont
+    if (contTemp>contFinal){
+        contFinal=contTemp
+        letra=nombre.charAt(index);
+    
+    }
+
+    
+}  //for index
+console.log(letra, contFinal);
